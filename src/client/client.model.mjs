78e12421ecffde1c7d55b76c7592a9e736358c3f
farlan_fadillah <farlanf61@gmail.com>
2026-03-06 20:34:27 +0700

    deleting src/client/client.model.mjs (unused file)

--- src/client/client.model.mjs
+++
@@ -1,8 +0,0 @@
-const model = {
-    string : [
-        "nik", "first_name", "last_name", "birth_place", "job_name",
-        "address", "address_code", "rt", "rw", "phone_number", "created_at",
-        "updated_at"    
-    ],
-
-}
